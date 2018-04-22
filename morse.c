5cec3b175d9181cbcf7b34d856b7ee4ffffcb958
rory <rory@ARCHBOX.etown.edu>
2018-04-22 12:47:41 +0000

    added comments about project (description, author, etc)

--- morse.c
+++ morse.c
@@ -1,3 +1,13 @@
+/****************************************************
+*
+*   Project Name:   morse
+*   Decription:     Turns an ascii phrase into a morse code string, then generates a morse code wav file
+*   Filenames:      morse.c, morse.h, dash.wav, dot.wav
+*   Author:         rory
+*
+****************************************************/
+
+/* imports */
 #include "morse.h"
 
 /*
